966f26c84b9bae8907224d72769b82d9cf4446cf
amine <aminear220@gmail.com>
2025-12-15 12:25:48 +0100

    Niveau Beginner

--- ex1.js
+++ ex1.js
@@ -165,6 +165,7 @@ console.log(" moyane = " +  somme / i);
 
 
 //8. Trouver un nombre donné (ex : 77)
+console.log( " Trouver un nombre donné (ex : 77)" );
 
 
 
@@ -175,7 +176,7 @@ while (i < data.length){
      if (data[i]  ===  number){
 
         console.log("nombre donné ");
-        return ;
+        
         
      }
 
@@ -191,14 +192,84 @@ while (i < data.length){
 
 
 i = 0; 
+j = 0;
+
+const arry = [];
 while (i < data.length){
 
-     if (data[i] ){
-
-        console.log("nombre donné ");
-        return ;
+     if (data[i]  > 20){
+        arry[j] = data[i];
+        j++;
         
      }
+    i++;
+    
+}
+
+console.log("les nombres supérieurs à 20 = " +  arry)
+
+
+
+// Vérifier si tous les nombres sont positifs
+console.log(" Vérifier si tous les nombres sont positifs")
+
+i = 0;
+while (i < data.length){
+
+     if (data[i]  > 0){
+        
+        i++;
+        
+     }else {
+        console.log("Tous les numéros de repas ");
+     }
+    
+    
+}
+
+
+
+//Vérifier si au moins un nombre est supérieur à 100
+console.log("Vérifier si au moins un nombre est supérieur à 100");
+
+ number = 100 ;
+i = 0; 
+j = 0;
+while (i < data.length){
+
+     if (data[i]  >  number){
+        j++;
+
+        console.log("existant");
+        
+        
+     }
+     
+    i++;
+    
+}
+console.log("Le nombre de nombres présents est supérieur à 100 = " +  j)
+
+
+
+// Compter combien de nombres sont > 50
+
+console.log(" Compter combien de nombres sont > 50");
+
+ number = 50 ;
+i = 0; 
+
+while (i < data.length){
+
+     if (data[i]  >  number){
+        if (data[i]  === 60){
+            console.log("existant");
+
+            
+        }
+    
+     }
+     
     i++;
     
 }
